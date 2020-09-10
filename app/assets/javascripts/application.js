86ae7cf481f37ce8128d6b7905ad5703dcd667d0
Nroulston <nicolas.roulston@gmail.com>
2020-09-09 20:35:15 -0700

    some basic jquery adding messages to the dom.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+
+// Try using this with rails. You check if turbolinks is loaded not the DomContent
+
+// $(document).on('turbolinks:load'), function() {
+//   // jquery examples on how to select and append
+//   $('#messages_table').append("hello, world!")
+// }
+
+//refactor it into a function of it's own concern this is all jquery right now.
+
+messageAppender = function(content) {
+  $('#messages-table').append(`${content}`)
+}
+
+$(document).on('DOMContentLoaded', function() {
+  // jquery examples on how to select and append
+  messageAppender("hello world");
+});
